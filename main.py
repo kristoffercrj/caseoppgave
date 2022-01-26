ba9f109af8946a65d42306c571b99f2dbbeb5b5c
kristoffercrj <76255658+kristoffercrj@users.noreply.github.com>
2022-01-26 23:20:59 +0100

    Updated code and added example images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import sys
+import argparse
 import matplotlib.pyplot as plt
 
 '''
@@ -12,29 +12,39 @@ category: string
 # Create a variable which represents the degree sign
 degree_sign = u"\N{DEGREE SIGN}"
 
-# To control that the user enters the correct amount of arguments
-if (len(sys.argv) > 4):
-    print('You have entered too many arguments.')
-    sys.exit()
+# Create parser
+parser = argparse.ArgumentParser()
+parser.add_argument("latitude", type = float, help = "Specify the desired north coordinate you want to get weather data from. This is a float value between -90 and 90.")
+parser.add_argument("longitude", type = float, help = "Specify the desired east coordinate you want to get weather data from. This is a float value between -180 and 180")
+parser.add_argument("category", type = str, help = "Choose between the following categories: \n temperature_2m")
 
-elif (len(sys.argv) < 2):
-    print(' You have entered too few arguments.')
-    sys.exit()
+args = parser.parse_args()
 
-# Inputs for the code to run
-latitude = sys.argv[1] # Input parameters for the user to write in latitude and longitude
-longitude = sys.argv[2] 
-category = sys.argv[3] # Input parameter to decide which category to display
+# Positional inputs that the user needs to add for the code to run
+latitude = args.latitude # Input parameters for the user to write in latitude and longitude
+longitude = args.longitude 
+category = args.category # Input parameter to decide which category to display
+
+print(f'You have entered the coordinates: ({latitude}N, {longitude}E) \
+\nSelected category for weather data: {category}')
 
 # Extract information from the API
 resp = requests.get(f'https://api.open-meteo.com/v1/forecast?latitude={float(latitude)}&longitude={float(longitude)}&hourly={category}')
 
 # Json parsing
+generation_time = resp.json()['generationtime_ms'] # Extracts the time spent generating the weather data
 y_values = resp.json()['hourly'][f'{category}'] # Extracts the y-values of the weather data
 metric = resp.json()['hourly_units'][f'{category}'] # Extracts the metric of the weather data
+
+print(f'Generating weather data for {category} at coordinates ({latitude}N, {longitude}E) ...')
+print(f'Finishing generating after {round(generation_time, 4)} ms!')
+print(f'Plotting data...')
 
 # Plotting of the graph
 plt.plot(y_values)
 plt.ylabel(metric)
+plt.xlim([0, 167])
+plt.xlabel('hours')
 plt.title(f'{category} at {latitude}{degree_sign}N {longitude}{degree_sign}E')
+print(f'Success!')
 plt.show()
